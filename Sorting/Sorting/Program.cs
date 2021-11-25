01caec16097e4c99ab3d3b3a994eb87d1015c220
suraj9982 <surajsk9982@gmail.com>
2021-11-25 21:44:46 +0530

    [suraj] add : this function sort element in bubble sort

---
+++ Sorting/Sorting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 100, 200, 50, 700, 500, 400 };
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("CHOOSE ONE OPTION FOR SORT\n1.BubbleSort\n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Sort.Bubble(numbers);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Sorting/Sorting/Sort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class Sort
+    {
+        public static void Bubble(int[] number)
+        {
+            int temp;
+            int length = number.Length;
+            for(int i = 0; i < length; i++)
+            {
+                for(int j = 0; j < length-1; j++)
+                {
+                    if (number[j] > number[j + 1])
+                    {
+                        temp = number[j];
+                        number[j] = number[j + 1];
+                        number[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("after sorting the number");
+            for(int i=0;i<length;i++)
+            {
+                Console.WriteLine(number[i] + " ");
+            }
+        }
+    }
+}
